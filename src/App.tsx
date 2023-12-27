12a879e1ee1ee18dc6541454a68a2d07704dc7bd
Gustavo Marques da Silva <programador05@iilex.com.br>
2023-12-27 19:18:59 -0300

    feat: add global styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import styled from "styled-components"
+import GlobalStyles from "./Components/GlobalStyles/Index"
+
+const BackgroundGradient = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244f 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
 function App() {
   return (
-    <>
-      
-    </>
+    <BackgroundGradient>
+      <GlobalStyles/>
+    </BackgroundGradient>
   )
 }
 
